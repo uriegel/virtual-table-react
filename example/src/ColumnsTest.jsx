f870aba11845eeebf2e1d9d20cb6e8453ff5491a
Uwe Riegel <uriegel@hotmail.de>
2021-02-04 22:41:44 +0100

    Colums as state

--- example/src/ColumnsTest.jsx
+++ example/src/ColumnsTest.jsx
@@ -1,24 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { Columns } from 'virtual-table-react'
 
 const App = () => {
+    const [cols, setCols] = useState([
+        [ "Eine Spalte", 1], 
+        [ "Zweite. Spalte", 2], 
+        [ "Letzte Spalte", 3]])
+
     return (
-        <table>
-            <Columns />
-            <tbody>
-                <tr>
-                    <td>Test</td>
-                    <td>txt</td>
-                    <td>25.02.1999 14:23</td>
-                </tr>
-                <tr>
-                    <td>Bild</td>
-                    <td>jpg</td>
-                    <td>15.12.2009 12:39</td>
-                </tr>                
-            </tbody>  
-        </table>
+        <div>
+            <p><button onClick={() => setCols([
+                [ "Column 1", 4], 
+                [ "2", 5 ], 
+                [ "3rd Col.", 6]])}>Ändern</button></p>
+            <table>
+                <Columns cols={cols} />
+                <tbody>
+                    <tr>
+                        <td>Test</td>
+                        <td>txt</td>
+                        <td>25.02.1999 14:23</td>
+                    </tr>
+                    <tr>
+                        <td>Bild</td>
+                        <td>jpg</td>
+                        <td>15.12.2009 12:39</td>
+                    </tr>                
+                </tbody>  
+            </table>
+        </div>
     )
 }
 
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react'
 import styles from './styles.module.css'
 
-export const Columns = ({ text }) => {
-	const cols = ["Eine Spalte", "Zweite. Spalte", "Letzte Spalte"]
+export const Columns = ({ cols }) => {
   	return (
 		<thead>
 			<tr>
-				{cols.map((col, i) => (<th key={i}>{col}</th> ))}
+				{cols.map((col, i) => (<th key={i}>{col[1]}</th> ))}
 			</tr>
 		</thead>
 	)
