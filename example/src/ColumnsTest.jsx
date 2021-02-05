08ec98153d5736fb82b39dd7ca535f14549e4adb
Uwe Riegel <uriegel@hotmail.de>
2021-02-05 07:08:02 +0100

    Styles

--- example/src/ColumnsTest.jsx
+++ example/src/ColumnsTest.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import 'virtual-table-react/dist/index.css'
 
 import { Columns } from 'virtual-table-react'
 
--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import styles from './styles.module.css'
 export const Columns = ({ cols }) => {
   	return (
 		<thead>
-			<tr>
-				{cols.map((col, i) => (<th key={i}>{col[1]}</th> ))}
+			<tr className={styles.pointerEw}>
+				{cols.map((col, i) => (
+					<th key={i} className={styles.column} >
+						{col[1]}
+					</th> 
+				))}
 			</tr>
 		</thead>
 	)
