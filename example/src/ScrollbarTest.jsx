a2cf67e1787e3630c763976843ea8131a68f05dd
Uwe Riegel <uriegel@hotmail.de>
2021-02-05 17:05:30 +0100

    itemsHeight itemsPerPage

--- example/src/ScrollbarTest.jsx
+++ example/src/ScrollbarTest.jsx
@@ -1,11 +1,31 @@
-import React, { useState } from 'react'
+import React, { useState, useRef } from 'react'
 import 'virtual-table-react/dist/index.css'
 
 //import { Columns } from 'virtual-table-react'
 
+const itemHeight = 18
+
 const App = () => {
+    const list = useRef(null);
+    const [isRunning, setIsRunning ] = useState(false)
+    const [height, setHeight ] = useState(0)
+    const [itemsPerPage, setItemsPerPage ] = useState(0)
     const [items, setItems ] = useState([])
     const onInputChange = e => setItems(Array.from(Array(parseInt(e.target.value)).keys()).map((n, i) => `Item # ${i}`))
+
+    const onResize = () => {
+        setHeight(list.current.clientHeight)
+        console.log(height)
+        setItemsPerPage(Math.floor(list.current.clientHeight / itemHeight))
+        console.log(itemsPerPage)
+    }
+
+    if (!isRunning) {
+        window.addEventListener("resize", onResize)
+        setIsRunning(true)
+        setTimeout(onResize)
+    }
+
     return (
         <div className='main'>
             <h1>Scrollbar Test</h1>
@@ -14,7 +34,7 @@ const App = () => {
                     <div>Message is: {items.length} </div>
                 </div>
             <div className='listcontainer'>
-            <div className="list" >
+            <div className="list" ref={list} >
                 {items.map((item) => (
                     <div key={item}>{item}</div> 
                 ))}
