668c2876be9752be35c7ebeabc255bd32f359956
Uwe Riegel <uriegel@hotmail.de>
2021-02-06 09:13:54 +0100

    Listbox and app separeated

--- example/src/ScrollbarTest.jsx
+++ example/src/ScrollbarTest.jsx
@@ -5,14 +5,12 @@ import { Scrollbar } from 'virtual-table-react'
 
 const itemHeight = 18
 
-const App = () => {
+const ListBox = ({items}) => {
     const list = useRef(null);
     const [isRunning, setIsRunning ] = useState(false)
     const [height, setHeight ] = useState(0)
     const [itemsPerPage, setItemsPerPage ] = useState(0)
-    const [items, setItems ] = useState([])
-    const onInputChange = e => setItems(Array.from(Array(parseInt(e.target.value)).keys()).map((n, i) => `Item # ${i}`))
-
+    
     const onResize = () => {
         setHeight(list.current.clientHeight)
         console.log(height)
@@ -27,20 +25,29 @@ const App = () => {
     }
 
     return (
-        <div className='main'>
-            <h1>Scrollbar Test</h1>
-                <div>
-                    <input type="number" onChange={onInputChange} placeholder="Items count" />
-                    <div>Message is: {items.length} </div>
-                </div>
-            <div className='listcontainer'>
+        <div className='listcontainer'>
             <div className="list" ref={list} >
                 {items.map((item) => (
                     <div key={item}>{item}</div> 
                 ))}
             </div>
             <Scrollbar /> 
-            </div>
+        </div>
+    )
+}
+
+const App = () => {
+    const [items, setItems ] = useState([])
+    const onInputChange = e => setItems(Array.from(Array(parseInt(e.target.value)).keys()).map((n, i) => `Item # ${i}`))
+
+    return (
+        <div className='main'>
+            <h1>Scrollbar Test</h1>
+                <div>
+                    <input type="number" onChange={onInputChange} placeholder="Items count" />
+                    <div>Message is: {items.length} </div>
+                </div>
+            <ListBox items={items}/>
         </div>    
     )
 }
